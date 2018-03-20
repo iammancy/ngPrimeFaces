9b57318d2f9a028ad570d318a550d328ce62b96e
iammancy <iammancy@gmail.com>
2018-03-20 23:51:45 +0530

    Setup NgPrime Faces

---
+++ primeFaceUIapp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CardModule  } from 'primeng/card';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Button Poetry Collection';
+}
---
+++ primeFaceUIapp/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {CardModule} from 'primeng/card';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
